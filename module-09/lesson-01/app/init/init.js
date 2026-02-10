a2c09cdd1e2ddd419bcf0f86cd98099f9ff23b07
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-02-10 14:59:44 +0200

    update

--- module-09/lesson-01/app/init/init.js
+++ module-09/lesson-01/app/init/init.js
@@ -1,6 +1,6 @@
 import { renderNotes } from "../renders/render.js";
 import { loadLS } from "../utils/localStorage.js";
-import { STORAGE_KEY } from '../utils/variables.js';
+import { STORAGE_KEY } from "../utils/variables.js";
 import { store } from "../utils/store.js";
 import refs from "../utils/refs.js";
 import { setTheme } from "../settings/theme-settings.js";
@@ -9,18 +9,17 @@ import { onDeleteNote } from "../components/deleteNote.js";
 import { onThemeToggle } from "../settings/theme-settings.js";
 
 export default function init() {
-    setTheme(loadLS(STORAGE_KEY.appTheme));
+  setTheme(loadLS(STORAGE_KEY.appTheme));
 
-    // Listeners
-    refs.form.addEventListener("submit", onFormSubmit);
-    refs.notesList.addEventListener("click", onDeleteNote);
-    refs.themeToggle.addEventListener("click", onThemeToggle);
+  // Listeners
+  refs.form.addEventListener("submit", onFormSubmit);
+  refs.notesList.addEventListener("click", onDeleteNote);
+  refs.themeToggle.addEventListener("click", onThemeToggle);
 
-    const notesData = loadLS(STORAGE_KEY.notes);
+  const notesData = loadLS(STORAGE_KEY.notes);
 
-    if (notesData !== undefined) {
+  if (notesData !== undefined) {
     store.notes = [...notesData];
     renderNotes(notesData);
-    }
-
+  }
 }
