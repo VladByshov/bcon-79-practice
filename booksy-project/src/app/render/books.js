2142c10310c37bd8598179e1fc8823784d4c23ac
OleksandrProtchenko <alexdevelop92@gmail.com>
2026-02-11 13:11:32 +0200

    update #5 book project

--- booksy-project/src/app/render/books.js
+++ booksy-project/src/app/render/books.js
@@ -5,6 +5,7 @@ export function renderBooks(books = [], el) {
   const markup = normalizedBooks
     .map(
       ({
+        _id,
         title,
         author,
         price,
@@ -13,14 +14,14 @@ export function renderBooks(books = [], el) {
         book_image_height,
       }) => {
         return `
-        <article class="books-item">
+        <article class="js-book-card books-item" data-book-id="${_id}">
             <div class="books-item-cover">
                 <img class="books-item-ill" src="${book_image}" alt="${title}" width="${book_image_width}" height="${book_image_height}" />
             </div>
             <div class="books-item-body">
                 <h3 class="books-item-title">${title}</h3>
                 <p class="books-item-descr">${author}</p>
-                <p class="books-item-price">${price}}</p>
+                <p class="books-item-price">${price}</p>
             </div>
             <button class="books-item-btn" type="button">Learn More</button>
         </article>
